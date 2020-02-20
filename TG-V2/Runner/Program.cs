93f0584c6b832452bcb844014cf7d32babd5062b
acvcmaster <acvcnos@hotmail.com>
2020-02-20 18:26:36 -0300

    Fazendo carta do baralho

---
+++ TG-V2/Runner/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SM;
+
+namespace Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card a = new dynamic[] {FaceValue.Ace, Suit.Spades};
+            Console.WriteLine(a.Name);
+        }
+    }
+}
---
+++ TG-V2/SM/Card.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace SM
+{
+    public enum Suit: int
+    {
+        Diamonds = 1,
+        Spades,
+        Hearts,
+        Clubs
+    }
+
+    public enum FaceValue: int
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Queen,
+        Jack,
+        King
+    }
+
+    public class Card
+    {
+        public FaceValue FaceValue { get; set; } = FaceValue.Ace;
+        public Suit Suit { get; set; } = Suit.Diamonds;
+
+        public string Name
+        {
+            get { return $"{this.FaceValue.ToString()} of {this.Suit.ToString()}"; }
+        }
+
+        public int BlackjackValue 
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public static implicit operator Card(dynamic[] values)
+        {
+            return new Card();
+        }
+    }
+}
