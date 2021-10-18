8d4b2d6be67b16f9eb49eef2eb04b9563b249a51
acvcmaster <acvcnos@hotmail.com>
2021-10-18 02:35:45 -0300

    Adding diagrammer (TG-V4)

---
+++ TG-Visualize/Diagrammer.cs
@@ -0,0 +1,75 @@
+using System.IO;
+using System.Text;
+
+public static class Diagrammer
+{
+    private delegate string LabelFunction(int rowIndex);
+
+    public static string Generate(char[] strategy, string name)
+    {
+        StringBuilder html = null;
+        using (StreamReader reader = new StreamReader("Templates/Diagram.html"))
+        {
+            html = new StringBuilder(reader.ReadToEnd());
+            var hardHands = GenerateHardHands(strategy);
+            html.Replace("#hardhands#", hardHands);
+
+            var softhands = GenerateSoftHands(strategy);
+            html.Replace("#softhands#", softhands);
+
+            var pairs = GeneratePairs(strategy);
+            html.Replace("#pairs#", pairs);
+
+            // Propriedades
+            html.Replace("#name#", name);
+        }
+
+        return html.ToString();
+    }
+
+    private static string GenerateHardHands(char[] strategy) => GenerateGeneric(strategy, 0, 160, (i) => $"{20 - i}");
+
+    private static string GenerateSoftHands(char[] strategy) =>
+        GenerateGeneric(strategy, 160, 240, (i) => $"A-{9 - i}");
+
+    private static string GeneratePairs(char[] strategy) =>
+        GenerateGeneric(strategy, 240, 340, (i) =>
+        {
+            if (i < 2) { return i == 0 ? "A-A" : "T-T"; }
+            else return $"{11 - i}-{11 - i}";
+        });
+
+    private static string GenerateGeneric(char[] strategy, int startIndex, int endIndex, LabelFunction function)
+    {
+        StringBuilder hardhands = new StringBuilder();
+        for (int i = startIndex; i < endIndex; i++)
+        {
+            var relativeIndex = i - startIndex;
+            if (relativeIndex % 10 == 0)
+            {
+                if (relativeIndex != 0)
+                    hardhands.Append("</tr>\n");
+                hardhands.Append("<tr>\n");
+                hardhands.Append($"<th>{function(relativeIndex / 10)}</th>\n");
+            }
+
+            switch (strategy[i])
+            {
+                case 'H':
+                    hardhands.Append($"<th value=\"H\"></th>\n");
+                    break;
+                case 'S':
+                    hardhands.Append($"<th value=\"S\"></th>\n");
+                    break;
+                case 'D':
+                    hardhands.Append($"<th value=\"D\"></th>\n");
+                    break;
+                case 'P':
+                    hardhands.Append($"<th value=\"P\"></th>\n");
+                    break;
+            }
+        }
+        hardhands.Append("</tr>\n");
+        return hardhands.ToString();
+    }
+}
---
+++ TG-Visualize/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace TG_Visualize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = args?.Length > 0 ? args[0] : null;
+            var name = !string.IsNullOrWhiteSpace(file) ? Path.GetFileName(file) : null;
+            var nameWithoutExtension = !string.IsNullOrWhiteSpace(file) ? Path.GetFileNameWithoutExtension(file) : null;
+
+            if (!string.IsNullOrWhiteSpace(file))
+            {
+                if (File.Exists(file))
+                {
+                    try
+                    {
+                        using (StreamReader reader = new StreamReader(file))
+                        {
+                            var data = reader.ReadToEnd();
+                            var possibleMoves = data?.Split(',');
+                            if (possibleMoves?.Length == 340)
+                            {
+                                var strategy = new char[340];
+                                for (var i = 0; i < 340; i++)
+                                {
+                                    var move = possibleMoves[i];
+                                    if (move.Length == 1)
+                                    {
+                                        strategy[i] = move[0];
+                                        using (var writer = new StreamWriter($"{nameWithoutExtension}.html"))
+                                        {
+                                            var html = Diagrammer.Generate(strategy, name);
+                                            writer.Write(html);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"Invalid move detected on file '{name}'. Offending value: '{move}'.");
+                                        break;
+                                    }
+                                }
+                            }
+                            else
+                                Console.WriteLine($"Invalid stategy detected on file '{name}'. Invalid length ({possibleMoves?.Length ?? 0} but expected 340).");
+                        }
+                    }
+                    catch (Exception error)
+                    {
+                        Console.WriteLine($"The conversion problem threw an error: {error?.Message}");
+                    }
+                }
+                else
+                    Console.WriteLine($"No such file '{file}'.");
+            }
+            else
+                Console.WriteLine("No file provided!");
+        }
+    }
+}
