b6ca247c2dd878227ad1f9630838d88458f4796b
acvcmaster <acvcnos@hotmail.com>
2021-10-26 05:49:02 +0000

    Adicionando dados coeff_var

---
+++ TG-V2/Runner/Data/coeff_var.m
@@ -0,0 +1,14 @@
+# Cross-section
+
+function curva_aprendizado()
+    data = load('coeff_var.csv');
+
+    x = data(:, 1);
+    z = data(:, 2);
+
+    plot(x, z);
+    title("Variabilidade da aptidão (50 amostras)");
+    xlabel("Número de episódios");
+    ylabel("Coeficiente de variação (CV)");
+    pause();
+endfunction
